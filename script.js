9cef069f5381519edd6259017d46033e4036e5a6
Av720 <120581801+Av720@users.noreply.github.com>
2023-02-18 20:54:03 -0700

    added the save when cliking a button function(functional)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+
+  console.log("ready to code")
+})
+
+//------------------------------------------------------------------------
 
 // display the time at top
 
@@ -9,92 +15,74 @@ setInterval(function () {
 }, 1000)
 
 console.log(today)
-//------------------------------------------------------------------------
-// create a global variable and define it
-
-var saveBtn = $(".saveBtn");
 
 //------------------------------------------------------------------------
 
 //create a function to tell the past present or future times on the calendar
 
-function currentTime() {
-  var currentHour = moment().hour();
-
-
-
-  $(".time-block").each(function () {
-    var calendarHour = parseInt($(this).attr("id"));
-
-    //create and if and else for the past present and future time blocks
-
-    if (currentHour < calendarHour) {
-      $(this).addClass("past");
-      $(this).removeClass("present");
-      $(this).removeClass("future");
-    }
-    else if (blockHour === currentHour) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-      $(this).removeClass("future");
-    }
-    else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    }
-
-  })
-  return (currentTime)
-};
+// function currentTime() {
+var currentHour = moment().format("HH");
 console.log(currentHour)
 
 
-//------------------------------------------------------------------------
+$(".time-block").each(function () {
+  var calendarHour = $(this).attr("id").split("-")[1];
+  console.log(calendarHour)
+  // console.log(calendarHour)
+  //create and if and else for the past present and future time blocks
+  if (currentHour == calendarHour) {
+    $(this).addClass("present");
+    $(this).children(".description").addClass("present");
 
-//create a function so that when the page refreshes , the data will save 
+  } else if (currentHour < calendarHour) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
 
-function saveOnRefresh() {
-
-  $(".hour").each(function () {
-
-    var currentHour = $(this).text();
-    var currentMeeting = localStorage.getItem(currentHour);
-
-    console.log(this);
-    console.log(currentHour);
-
-    if (currentMeeting !== null) {
-      $(this).siblings(".description").val(currentMeeting);
-    }
-
-  });
-}
-
-
-//------------------------------------------------------------------------
-
-// create a function for the evenlistener and localstorage
-
-$(document).ready(function () {
-
-  // create an eventlistener for the save button
-  $(".saveBtn").on("click", function () {
-
-    //create local variables for the local storage
-    var localStoragetTime = $(this).parent().attr("id");
-    var localStorageMeeting = $(this).siblings(".description").val();
-
-    // saving the text and time in the local storage by using setItem
-    localStorage.setItem(localStoragetTime, localStorageMeeting);
-
-    //console log both vars to make sure they are working 
-    console.log(localStorageMeeting, localStoragetTime)
-  }) // end of function
-
-  //------------------------------------------------------------------------
-
+  } else if (currentHour > calendarHour) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
 });
+
+//------------------------------------------------------------------------
+
+
+// save the data to the local storage and event listener
+
+$(".saveBtn").click(function () {
+
+  var localTime = $(this).siblings(".hour").text();
+  var localPlan = $(this).siblings(".description").val();
+
+  localStorage.setItem(localTime, localPlan);
+});
+
+//------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
