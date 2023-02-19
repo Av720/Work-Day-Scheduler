ec258c6e1b9b7bb2be55efb140e1cfd996aca0e1
Av720 <120581801+Av720@users.noreply.github.com>
2023-02-18 21:52:53 -0700

    added favicon and Pseudo code for JS

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 
-  console.log("ready to code")
+  // console.log("ready to code")
+
+  // this will run the function (saveOnRefresh)
+  saveOnRefresh()
+  // console.log(localStorage)
 })
 
 //------------------------------------------------------------------------
@@ -20,17 +24,17 @@ console.log(today)
 
 //create a function to tell the past present or future times on the calendar
 
-// function currentTime() {
+// this function will tell the current time using momemt
 var currentHour = moment().format("HH");
-console.log(currentHour)
+// console.log(currentHour)
 
-
+// traverse the DOM
 $(".time-block").each(function () {
   var calendarHour = $(this).attr("id").split("-")[1];
 
-  // console.log(calendarHour)
+  // console.log(calendarHour) = works
 
-  //create and if and else for the past present and future time blocks
+  //create and if and else for the past present and future time blocks, this will change the colors according to the current hour present 
   if (currentHour == calendarHour) {
     $(this).addClass("present");
     $(this).children(".description").addClass("present");
@@ -50,30 +54,35 @@ $(".time-block").each(function () {
 
 // save the data to the local storage and event listener
 
+//create the functions using the saveBtn class and add click event listener
 $(".saveBtn").click(function () {
 
+  //create 2 variables , 1 for the time and the other for the value > then tie them to the ID/class
   var localTime = $(this).siblings(".hour").text();
   var localPlan = $(this).siblings(".description").val();
 
+
+  //create the local storage and use setItem 
   localStorage.setItem(localTime, localPlan);
 });
 
 //-----------------------------------------------------------------------
-
-var saveBtn = $(".saveBtn");
 
 
 // //   //create a function so that when the page refreshes , the data will save 
 
 function saveOnRefresh() {
 
+  //create the function using the class .hour 
   $(".hour").each(function () {
 
+
+    //create 2 vars for each value 
     var currentHour = $(this).text();
     var currentMeeting = localStorage.getItem(currentHour);
 
-    console.log(this);
-    console.log(currentHour);
+    // console.log(this); = works
+    // console.log(currentHour); = works
 
     if (currentMeeting !== null) {
       $(this).siblings(".description").val(currentMeeting);
@@ -83,6 +92,7 @@ function saveOnRefresh() {
 }
 
 
+//-----------------------------------------------------------------------
 
 
 
